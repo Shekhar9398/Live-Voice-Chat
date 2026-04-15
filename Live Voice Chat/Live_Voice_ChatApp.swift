addf11d5d340c326087cb555ed6eceecec8f7e17
Shekhar Manwar <shekhar.manwar@thinkitive.com>
2026-04-15 16:10:28 +0530

    Initial Commit

---
+++ Live Voice Chat/Live_Voice_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_Voice_ChatApp.swift
+//  Live Voice Chat
+//
+//  Created by Mac on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Live_Voice_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
